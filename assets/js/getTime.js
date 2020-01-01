d95d0625657d52923250fcec0370b5b10441506a
NekomoQ <Nekomohu2222+NekomoQ@gmail.com>
2020-01-01 20:01:04 +0900

    NOBRUH

--- assets/js/getTime.js
+++ assets/js/getTime.js
@@ -5,7 +5,7 @@ var t = new XMLHttpRequest;
 	t.onreadystatechange = function() {
 		if (4 == t.readyState && 200 == t.status) {
 			var e = JSON.parse(t.responseText),
-			var n = new Date(1e3 * e.st);
+			n = new Date(1e3 * e.st);
 			x = n.toLocaleString()
 		}
 	}, t.open("GET", "https://ntp-a1.nict.go.jp/cgi-bin/json", !1), t.send(null)
