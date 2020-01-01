7693fcdda72d3c88cfc38c64a735892b648ddb37
NekomoQ <Nekomohu2222+NekomoQ@gmail.com>
2020-01-01 19:44:07 +0900

    ???w

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,13 +1,19 @@
-self.addEventListener('msg', function(e) {
-window.onload=fClock=function(){var t=new XMLHttpRequest;t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){var e=JSON.parse(t.responseText),n=new Date(1e3*e.st);document.getElementById("clock").textContent=n.toLocaleString()}},t.open("GET","https://ntp-a1.nict.go.jp/cgi-bin/json",!1),t.send(null)},setInterval(fClock,950);window.onload = fClock = function() {
-	var t = new XMLHttpRequest;
+var x = 'N/A';
+var worker = new Worker('assets/js/clock.js');
+
+worker.addEventListener('msg', function(e) {
+    var t = new XMLHttpRequest;
 	t.onreadystatechange = function() {
 		if (4 == t.readyState && 200 == t.status) {
 			var e = JSON.parse(t.responseText),
 				n = new Date(1e3 * e.st);
-			document.getElementById("clock").textContent = n.toLocaleString()
+				document.getElementById("clock").textContent = x = n.toLocaleString()
 		}
 	}, t.open("GET", "https://ntp-a1.nict.go.jp/cgi-bin/json", !1), t.send(null)
+  }, false);
+  
+  
+  window.onload = fClock = function() {
+	worker.postMessage('a');
 }, setInterval(fClock, 950);
-self.postMessage(e.data);
-}, false);
+
