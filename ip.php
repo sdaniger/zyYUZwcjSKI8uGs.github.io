8d93ceaae25e47b230908f53a6a99c732ebd761e
NekomoQ <Nekomohu2222+NekomoQ@gmail.com>
2020-01-18 10:54:07 +0900

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="content-language" content="ja">
+  <meta name="robots" content="noindex,nofollow">
+  <meta charset="UTF-8">
+  <style type="text/css"></style>
+ </head>
+ <body>
+    <?php
+	$name= echo $_SERVER["REMOTE_ADDR"] ;
+	print ($name);
+	?>
+    </p>
+ </body>
+</html>
